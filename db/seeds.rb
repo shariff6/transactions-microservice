785115b68a5710c1a27d8db0deba986c9ae982f9
Mohammed Shariff <mohammed.shariff@mediapal.net>
2022-10-12 16:44:52 +0300

    Adds Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+transaction = Transaction.create(
+    [
+        {
+            user_id: 2345678,
+            input_amount: 100000,
+            input_currency: "USD",
+            output_amount: 12090000,
+            output_currency: "KES"
+        },
+        {
+            user_id: 2345678,
+            input_amount: 100000,
+            input_currency: "USD",
+            output_amount: 124515,
+            output_currency: "NGN"
+        }
+    ]
+)
