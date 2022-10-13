acd92a65ad878230174b506b06b8c22f8dc0b478
Mohammed Shariff <mohammed.shariff@mediapal.net>
2022-10-13 15:36:41 +0300

    Adds authentication via jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post 'authenticate', to: 'authentication#authenticate'
   namespace :api do
     resources :transactions, only: [:index, :show, :create]
+    resources :users, only: [:create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
