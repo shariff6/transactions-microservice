c9c68cd3c2481d36b156ccde7b00a05a4b726954
Mohammed Shariff <mohammed.shariff@mediapal.net>
2022-10-15 08:08:00 +0300

    Adds comments

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,11 +6,12 @@ class Transaction < ApplicationRecord
     validates :input_currency, presence: true
     validates :output_currency, presence: true
 
-
+# Calcultes output amount based on input and output_currency currency
     def output_amount()
         Money.from_cents(input_amount, input_currency).exchange_to(output_currency).fractional 
     end
 
+# Converts money to cents
     before_save do
         self.input_amount = input_amount * 100
     end
