0f8bc8621308736180f708618a5fa58876ad72ea
Mohammed Shariff <mohammed.shariff@mediapal.net>
2022-10-16 21:49:33 +0300

    Syntax error in transaction spec fixed

--- spec/requests/transaction_spec.rb
+++ spec/requests/transaction_spec.rb
@@ -1,17 +1,15 @@
-require 'rails_helper'
-require 'spec_helper'
+require "rails_helper"
+require "spec_helper"
 RSpec.describe "Transactions", type: :request do
-
-  # let(:user) { FactoryBot.create(:user, email: "Shariff.email.com", password: "23456789", password_confirmation: "23456789", name: "Shariff" ) }
+  # let(:user) { FactoryBot.create(:user, email: "Shariff@email.com", password: "23456789", password_confirmation: "23456789", name: "Shariff") }
+  # let(:transaction) { FactoryBot.create(user_id: user.id, input_amount: 1000, input_currency: "KES", output_currency: "USD") }
   # describe "GET /index" do
 
   #   # let(:admin) { users(:admin) }
-
-  #   before { get '/api/transaction', headers: { 'Authorization' => Authentication.call(user.id) } }
-
-  #   it 'is expected to assign transaction instance variable' do
-  #     expect(assigns[:transactions]).to eq(Transaction.all)
-  #   end
+  #   # get :index,  headers: { "Authorization" => AuthenticateUser.call(user.email, user.password).result }
+  #   { get "/api/transactions", headers: { "Authorization" => AuthenticateUser.call(user.email, user.password).result } }
+  #   expect(response).to be_successful
+  #   expect(response_json.size).to eq 1
+  #   expect(response_json.first["id"]).to eq todo.id
+  # end
 end
-end
-
